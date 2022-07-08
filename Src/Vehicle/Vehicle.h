afb4dae643d29567a8bf0c9c26dafbedb223f7c6
atlantis13579 <atlantis13579@msn.com>
2022-07-08 20:35:12 +0800

    add vehicle model

--- Src/Vehicle/Vehicle.h
+++ Src/Vehicle/Vehicle.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "../Maths/Quaternion.h"
+#include "../Maths/Vector3d.h"
+
+class VehicleModel
+{
+public:
+	Vector3d	GetPosition() const;
+	Quaternion	GetRotation() const;
+	Vector3d	GetFrontVector() const;
+	Vector3d	GetUpVector() const;
+	Vector3d	GetLeftVector() const;
+	Vector3d	GetRightVector() const;
+
+	float		GetSteerAngle() const;
+	float		GetMaxSteerAngle() const;
+
+	Vector3d	GetCenterOfMass() const;
+	Vector3d	GetFrontAxleCenter() const;
+	Vector3d	GetRearAxleCenter() const;
+	float		GetFrontAxleWidth() const;
+	float		GetReartAxleWidth() const;
+	float		GetAxleSeparation() const;
+};
