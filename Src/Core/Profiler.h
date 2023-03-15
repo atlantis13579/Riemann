d93e2836b2e68fed4a0b942673abf9b758122290
Fisher Zhang <atlantis13579@msn.com>
2023-03-15 12:39:18 +0800

    Update Profiler.h
    time

--- Src/Core/Profiler.h
+++ Src/Core/Profiler.h
@@ -20,3 +20,51 @@ public:
 private:
 	std::chrono::steady_clock::time_point tp_begin_;
 };
+
+class WorldTime
+{
+public:
+	WorldTime()
+	{
+		InitStartTime();
+	}
+
+	void	Advanced(double dt)
+	{
+		milliseconds += dt;
+		if (milliseconds >= 1.0)
+		{
+			seconds += (int)milliseconds;
+			milliseconds = fmod(milliseconds, 1.0);
+			if (fabs(milliseconds < 1e-6))
+			{
+				milliseconds = 0.0;
+			}
+		}
+	}
+
+	void	InitStartTime()
+	{
+		seconds = 0;
+		milliseconds = 0.0;
+		start_time = std::chrono::steady_clock::now();
+	}
+
+	double	GetTimeSeconds() const
+	{
+		const double time = (double)seconds + milliseconds;
+		return time;
+	}
+
+	double	GetSysTimeSeconds() const
+	{
+		std::chrono::steady_clock::time_point curr = std::chrono::steady_clock::now();
+		const double time = (double)std::chrono::duration_cast<std::chrono::seconds>(curr - start_time).count();
+		return time;
+	}
+
+private:
+	std::chrono::steady_clock::time_point	start_time;
+	int		seconds;
+	double	milliseconds;
+};
