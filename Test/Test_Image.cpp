7c7aeaac907e22ff85940c0c79972495ec120399
atlantis13579 <atlantis13579@msn.com>
2024-03-06 05:10:10 +0800

    rm files

--- Test/Test_Image.cpp
+++ Test/Test_Image.cpp
@@ -3,29 +3,8 @@
 
 #include "Test.h"
 
-#include "../Src/Modules/Tools/ContinuousBitmap.h"
-
 // using namespace Riemann;
-
-void TestBitmap()
-{
-	printf("Running TestBitmap\n");
-	int a[] = { 1, 1, 0, 0, 1, 0, 1, 0,
-				1, 1, 1, 1, 1, 1, 1, 1,
-				0, 0, 0, 0, 0, 0, 0, 0,
-				0, 1, 0, 0, 1, 0, 1, 1 };
-	ContinuousBitmap<uint16_t> bitmap;
-	bitmap.Build<int>(a, 8, 4, -4, -4, 4, 4);
-	bitmap.SerializeToFile("../TestData/cbit.map");
-	bitmap.SerializeFromFile("../TestData/cbit.map");
-	EXPECT(bitmap.QueryBitmapSpace(3, 0) == false);
-	EXPECT(bitmap.QueryBitmapSpace(4, 0) == true);
-	EXPECT(bitmap.QueryBitmapSpace(3, 1) == true);
-	EXPECT(bitmap.QueryBitmapSpace(13, 1) == false);
-	EXPECT(bitmap.QueryBitmapSpace(7, 3) == true);
-}
 
 void TestImage()
 {
-	TestBitmap();
 }
